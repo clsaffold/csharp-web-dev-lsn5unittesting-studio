6a8ee47dec85a8ac9f7f2bbd61fd03efebb5fb26
csaffold <ceciliasaffold@gmail.com>
2020-10-15 20:09:39 -0500

    Created seven tests; need to revise code to make sure brackets are listed in order open then closed

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BalancedBracketsNS;
+using System;
 
 namespace BalancedBracketsTests
 {
@@ -9,9 +10,51 @@ namespace BalancedBracketsTests
         // TODO: Add tests to this file.
 
         [TestMethod]
-        public void EmptyTest()
+        public void OnlyBracketsReturnsTrue()
         {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]"));
             Assert.AreEqual(true, true);
+        }
+        [TestMethod]
+        public void EmptyStringReturnsTrue()
+        {
+           Boolean result = BalancedBrackets.HasBalancedBrackets("");
+           Assert.AreEqual(result, true);
+        }
+        [TestMethod]
+        public void FullyWrappedBalancedStringReturnsTrue()
+        {
+            String input = "[LaunchCode]";
+            Boolean result = BalancedBrackets.HasBalancedBrackets(input);
+            Assert.AreEqual(result, true);
+        }
+        [TestMethod]
+        public void PartiallyWrappedBalancedStringReturnsTrue()
+        {
+            String input = "Launch[Code]";
+            Boolean result = BalancedBrackets.HasBalancedBrackets(input);
+            Assert.AreEqual(result, true);
+        }
+        [TestMethod]
+        public void FullyWrappedBalancedStringReturnsFalse()
+        {
+            String input = "[LaunchCode";
+            Boolean result = BalancedBrackets.HasBalancedBrackets(input);
+            Assert.AreEqual(result, false);
+        }
+        [TestMethod]
+        public void PartiallyWrappedBalancedStringReturnsFalse()
+        {
+            String input = "Launch[Code";
+            Boolean result = BalancedBrackets.HasBalancedBrackets(input);
+            Assert.AreEqual(result, false);
+        }
+        [TestMethod]
+        public void WrongNestedBracketReturnsFalse()
+        {
+            String input = "][";
+            Boolean result = BalancedBrackets.HasBalancedBrackets(input);
+            Assert.AreEqual(result, false);
         }
     }
 }
